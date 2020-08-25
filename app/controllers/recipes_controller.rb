6e5350264c5f2883f71505af8500d66be3a46c85
gzim921 <gzimiseni7@gmail.com>
2020-08-25 19:15:14 +0200

    Added Bulma and created partial view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,6 +21,18 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+    # @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :edit
+    end
+  end
+
   private
 
   def recipe_params
